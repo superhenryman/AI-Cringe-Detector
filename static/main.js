fc569879a9635898f17ec3aed321523514aa6c15
superhenryman <kareem.a.alhaj@gmail.com>
2025-07-16 05:26:31 +0300

    Starwalker

--- static/main.js
+++ static/main.js
@@ -5,22 +5,20 @@ form.addEventListener("submit", async (e) => {
     const formData = new FormData(form);
     const data = Object.fromEntries(formData.entries());
     const file = formData.get('file');
-    if (file instanceof File) {
-        if (file.type !== 'text/plain' && file.type !== 'image/png' && file.type !== 'image/jpeg') {
-            alert('Please upload a valid file type: .txt, .png, or .jpg');
-            return;
-        }
-    } else {
+    if (!(file instanceof File) || file.size === 0) {
         alert('Please select a file to upload.');
+        return;
+    }
+
+    const allowedTypes = ['text/plain', 'image/png', 'image/jpeg'];
+    if (!allowedTypes.includes(file.type)) {
+        alert('Please upload a valid file type: .txt, .png, or .jpg');
         return;
     }
     try {
         const response = await fetch('/cringeornot', {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
+            body: formData,
         });
 
         if (!response.ok) {
